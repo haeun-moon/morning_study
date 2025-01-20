2648da8a244973eac8175924c3a19ecb91f7a82e
haeun-moon <seenmoon12@gmail.com>
2025-01-21 02:27:59 +0900

    [haeun] unsolve algorithm02

---
+++ 1st_week/haeun/src/algorithm02.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class algorithm02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String number = sc.next();
+
+        int plus = 0;
+//        int[] numbers = {};
+
+        for (int i = 0; i < number.length(); ++i) {
+//            char[] numbers = new char[number.length()];
+//            int[] numbers = {};
+//            int[i] numbers = number.charAt(i);
+//            char a = number.charAt(i);
+//            System.out.println(a);
+            int[] numbers = new int[number.charAt(i)];
+//            int[] numbers = new int[]{number.charAt(i)};
+//            for (int j = 0; j < numbers.length(); ++j) {
+//            int plus;
+            System.out.println(numbers[i]);
+//            plus = ++numbers[i];
+
+
+
+        }
+//        System.out.println(plus);
+        }
+
+
+
+
+    }
+
+//}
